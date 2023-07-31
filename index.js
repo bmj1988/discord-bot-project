9cfcae56fe3f6ac660f6287a6bcc758370a2ca2c
Brian <cjcores@gmail.com>
2023-07-30 20:51:15 -0400

    some simple slash commands

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const {Configuration, OpenAIApi } = require("openai");
+/// NODE
+const fs = require('node:fs');
+const path = require('node:path');
+/// DOTENV
 const dotenv = require('dotenv');
 dotenv.config();
-const {Client, Events, GatewayIntentBits} = require('discord.js');
+//// Discord API
+const {Client, Events, Collection, GatewayIntentBits} = require('discord.js');
 const client = new Client({intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageTyping]
 });
+/// OpenAI API
+const {Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration)
+/// SLASHERS
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+const commandsPath = path.join(foldersPath, folder);
+const commandFiles = fs.readdirSync(commandsPath).filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+    }
+}
+}
 // const MDNSearch = require('./commands/mdn.js')
-const ping = require('./commands/ping.js')
+const ping = require('./commands/fun/ping.js')
 
 //chatgpt client section
 
@@ -43,7 +69,7 @@ const ping = require('./commands/ping.js')
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
-
+/// MESSAGE HANDLING
 client.on('messageCreate', incoming)
     function incoming (msg) {
     if (msg.content === 'hi') {
@@ -61,5 +87,30 @@ client.on('messageCreate', incoming)
             // msg.reply(response)
         }
     };
+
+/// SLASH HANDLER
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`No command: ${interaction.commandName} could be found.`)
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({ content: 'Could not execute command.', ephemeral: true })
+        }
+        else {
+            await interaction.reply({ content: 'Could not execute command.', ephemeral: true })
+        }
+    }
+    });
 
 client.login(process.env.DISCORD_TOKEN)
